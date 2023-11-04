47b37d5036a82346f3117447729fed27ae058bb2
PsychoZexta <zohaib.saeed1259@gmail.com>
2023-11-04 21:09:46 +0500

    Created home page and basic structure of the website

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Home from './Components/Home'
+
+export default function App() {
+  return (
+    <>
+    <Home />
+    </>
+  )
+}
---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Hero from './Hero'
+import Featured from './Featured'
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <Featured />
+        </>
+    )
+}
+
+export default Home
